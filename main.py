8f1b33fbfc14ccabfbe2c5a4c3d825da29923861
chrismytine <christineanastasia2011@gmail.com>
2025-02-26 18:15:10 +0800

    Update main.py

--- main.py
+++ main.py
@@ -1,43 +1,62 @@
 import discord
 from discord.ext import commands
 from config import token
-from logic import Pokemon
+from logic import Pokemon, Wizard, Fighter
+import random
 
-# Setting up intents for the bot
-intents = discord.Intents.default()  # Getting the default settings
-intents.messages = True              # Allowing the bot to process messages
-intents.message_content = True       # Allowing the bot to read message content
-intents.guilds = True                # Allowing the bot to work with servers (guilds)
-
-# Creating a bot with a defined command prefix and activated intents
+intents = discord.Intents.default()
+intents.messages = True
+intents.message_content = True
+intents.guilds = True
 bot = commands.Bot(command_prefix='!', intents=intents)
 
-# An event that is triggered when the bot is ready to run
 @bot.event
 async def on_ready():
-    print(f'Logged in as {bot.user.name}')  # Outputs the bot's name to the console
+    print(f'Logged in as {bot.user.name}')
 
-# The '!go' command
 @bot.command()
 async def go(ctx):
-    author = ctx.author.name  # Getting the name of the message's author
-    # Check whether the user already has a Pokémon. If not, then...
-    if author not in Pokemon.pokemons.keys():
-        pokemon = Pokemon(author)  # Creating a new Pokémon
-        await ctx.send(await pokemon.info())  # Sending information about the Pokémon
-        image_url = await pokemon.show_img()  # Getting the URL of the Pokémon image
+    author = ctx.author.name
+    if author not in Pokemon.pokemons:
+        chance = random.randint(1, 3)
+        if chance == 1:
+            pokemon = Pokemon(author)
+        elif chance == 2:
+            pokemon = Wizard(author)
+        elif chance == 3:
+            pokemon = Fighter(author)
+        await ctx.send(await pokemon.info())
+        image_url = await pokemon.show_img()
         if image_url:
-            embed = discord.Embed()  # Creating an embed message
-            embed.set_image(url=image_url)  # Setting up the Pokémon's image
-            await ctx.send(embed=embed)  # Sending an embedded message with an image
+            embed = discord.Embed()
+            embed.set_image(url=image_url)
+            await ctx.send(embed=embed)
         else:
-            await ctx.send("Failed to upload an image of the pokémon.")  
-        height = await pokemon.get_height() 
-        if height:
-            await ctx.send(f"The height of your Pokémon is {height} decimeters.") 
-        else:
-            await ctx.send("Failed to fetch the height of the Pokémon.")
+            await ctx.send("Gagal memuat gambar Pokémon.")
     else:
-        await ctx.send("You've already created your own Pokémon.")  # A message that is printed whether a Pokémon has already been created
-# Running the bot
+        await ctx.send("Anda sudah memiliki Pokémon!")
+
+@bot.command()
+async def attack(ctx):
+    target = ctx.message.mentions[0] if ctx.message.mentions else None
+    if target:
+        if target.name in Pokemon.pokemons and ctx.author.name in Pokemon.pokemons:
+            enemy = Pokemon.pokemons[target.name]
+            attacker = Pokemon.pokemons[ctx.author.name]
+            result = await attacker.attack(enemy)
+            await ctx.send(result)
+        else:
+            await ctx.send("Kedua pemain harus memiliki Pokémon untuk pertarungan!")
+    else:
+        await ctx.send("Tetapkan pemain yang ingin Anda serang dengan menyebutnya.")
+
+@bot.command()
+async def info(ctx):
+    author = ctx.author.name
+    if author in Pokemon.pokemons:
+        pokemon = Pokemon.pokemons[author]
+        await ctx.send(await pokemon.info())
+    else:
+        await ctx.send("Anda tidak memiliki Pokémon!")
+
 bot.run(token)
