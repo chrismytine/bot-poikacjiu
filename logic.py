2e797a9267c8674dd39702cee6223c6ce883921c
chrismytine <christineanastasia2011@gmail.com>
2025-03-05 13:22:18 +0800

    Update logic.py

--- logic.py
+++ logic.py
@@ -1,7 +1,8 @@
 import aiohttp
 import random
 from random import randint
-import asyncio  # Add asyncio to handle asynchronous code
+from datetime import datetime, timedelta
+
 
 class Pokemon:
     pokemons = {}
@@ -11,12 +12,21 @@ class Pokemon:
         self.pokemon_number = random.randint(1, 1000)
         self.name = None
         self.img = None
-        self.experience = 0
-        self.height = None
         self.power = random.randint(30, 60)
         self.hp = random.randint(200, 400)
+        self.last_feed_time  = datetime.now()
         if pokemon_trainer not in self.pokemons:
             self.pokemons[pokemon_trainer] = self
+
+    async def feed(self, feed_interval=60, hp_increase=10):
+        current_time = datetime.now()
+        delta_time = timedelta(seconds=feed_interval)
+        if (current_time - self.last_feed_time) > delta_time:
+            self.hp += hp_increase
+            self.last_feed_time = current_time
+            return f"Kesehatan pokemon meningkat. Kesehatan pokemon sekarang: {self.hp}"
+        else:
+            return f"Waktu makan pokemon berikutnya: {current_time+delta_time}"
 
     async def get_name(self):
         url = f'https://pokeapi.co/api/v2/pokemon/{self.pokemon_number}'
@@ -27,21 +37,13 @@ class Pokemon:
                     return data['forms'][0]['name']
                 else:
                     return "Pikachu"
-    async def gain_experience(self, xp_amount):
-        self.experience += xp_amount
-        level_up_exp = 100 * self.level 
-        while self.experience >= level_up_exp:
-            self.level += 1
-            self.experience -= level_up_exp
-            level_up_exp = 100 * self.level  
-            print(f"{self.name} has leveled up! Now at level {self.level}.")
 
     async def info(self):
         if not self.name:
             self.name = await self.get_name()
-        return f"""Nama Pokemon kamu: {self.name}
-                Kekuatan Pokemon: {self.power}
-                HP Pokemon: {self.hp}"""
+        return f"""Nama pokemon anda: {self.name}
+                Power pokemon anda: {self.power}
+                Kesehatan pokemon anda: {self.hp}"""
 
     async def show_img(self):
         url = f'https://pokeapi.co/api/v2/pokemon/{self.pokemon_number}'
@@ -58,18 +60,18 @@ class Pokemon:
         if isinstance(enemy, Wizard):
             chance = randint(1, 5)
             if chance == 1:
-                return "Pokemon Penyihir menggunakan perisai dalam pertarungan"
+                return "Pokemon Penyihir menggunakan perisai dalam pertempuran"
         if enemy.hp > self.power:
             enemy.hp -= self.power
-            return f"Pertarungan @{self.pokemon_trainer} melawan @{enemy.pokemon_trainer}\nHP @{enemy.pokemon_trainer} sekarang {enemy.hp}"
+            return f"Pertempuran @{self.pokemon_trainer} dengan @{enemy.pokemon_trainer}\nKesehatan @{enemy.pokemon_trainer} sekarang {enemy.hp}"
         else:
             enemy.hp = 0
-            return f"@{self.pokemon_trainer} menang melawan @{enemy.pokemon_trainer}!"
+            return f"@{self.pokemon_trainer} menang dari @{enemy.pokemon_trainer}!"
 
 
 class Wizard(Pokemon):
-    # Kelas ini dapat menambahkan method dan properti khusus untuk penyihir
-    pass
+    async def feed(self):
+        return await super().feed(hp_increase=20)
 
 
 class Fighter(Pokemon):
@@ -78,22 +80,8 @@ class Fighter(Pokemon):
         self.power += super_power
         result = await super().attack(enemy)
         self.power -= super_power
-        return result + f"\nPetarung menggunakan serangan super dengan kekuatan:{super_power}"
-
-
-# async def main():
-#     wizard = Wizard("username1")
-#     fighter = Fighter("username2")
-
-#     print(await wizard.info())  # Await here to get the result of the coroutine
-#     print("#" * 10)
-#     print(await fighter.info())  # Await here as well
-#     print("#" * 10)
-#     # You can also await attacks if needed
-#     print(await wizard.attack(fighter))
-#     print(await fighter.attack(wizard))
-
-
-# # Run the main function inside an event loop
-# asyncio.run(main())
+        return result + f"\nPokemon petarung menggunakan kekuatan serangan super:{super_power}"
+    
+    async def feed(self):
+        return await super().feed(feed_interval=10)
 
